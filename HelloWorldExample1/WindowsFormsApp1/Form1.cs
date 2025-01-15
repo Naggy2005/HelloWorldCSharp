d1ece65650307c1e358751ab45308de4749659e7
Naggy2005 <156240986+Naggy2005@users.noreply.github.com>
2025-01-14 19:44:51 -0500

    My First Version of Hello World
    Making a form and a car object.

---
+++ HelloWorldExample1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HelloWorldExample;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // make a car
+            Car myBMW = new Car("BMW", "325xi", 2006, "WA200522", "Red", "Leather");
+            //show info about the car on the label
+            label1.Text = myBMW.make.ToString();
+            List<Car> list = new List<Car>();
+        }
+    }
+}
